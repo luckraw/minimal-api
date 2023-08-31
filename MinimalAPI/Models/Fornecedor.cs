dc4d4609261fa708a3c078ebe538214ec2c5cfaf
null <krawdev@gmail.com>
2023-08-31 13:31:45 -0300

    ContextDb implement

--- MinimalAPI/Models/Fornecedor.cs
+++ MinimalAPI/Models/Fornecedor.cs
@@ -2,7 +2,7 @@
 {
     public class Fornecedor
     {
-        public Guid Guid { get; set; }
+        public Guid Id { get; set; }
         public string? Nome { get; set; }
         public string? Documento { get; set; }
         public bool Ativo { get; set; }
