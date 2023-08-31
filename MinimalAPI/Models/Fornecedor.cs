69e947dbb8b0c545d649a77a3c95c95ae4f41181
null <krawdev@gmail.com>
2023-08-31 12:49:27 -0300

    Adicionar arquivos de projeto.

---
+++ MinimalAPI/Models/Fornecedor.cs
@@ -0,0 +1,13 @@
+﻿namespace MinimalAPI.Models
+{
+    public class Fornecedor
+    {
+        public Guid Guid { get; set; }
+        public string? Nome { get; set; }
+        public string? Documento { get; set; }
+        public bool Ativo { get; set; }
+
+
+
+    }
+}
